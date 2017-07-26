7ba1065a81ffc7dc9d877aa1a89446b498fa36eb
andrew-code <djfanandrew@gmail.com>
2017-07-26 17:29:43 +0300

    створив клас

--- test_print_list.py
+++ test_print_list.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 
-driver = webdriver.Firefox()
-driver.get("http://www.airindia.in/")
-list_elements = driver.find_element_by_css_selector('div.footerLinks').text
-print(list_elements)
-driver.quit()
+class TestOne:
+    def test_print_list(self):
+        driver = webdriver.Firefox()
+        driver.get("http://www.airindia.in/")
+        list_elements = driver.find_element_by_css_selector('div.footerLinks').text
+        print(list_elements)
+        driver.quit()
+
